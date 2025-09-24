0b967a40e471e1b3e98ef9b87aae5dfd5a87baf0
Vishal <vishalsuji0711@gmail.com>
2025-09-24 19:51:27 +0530

    Update API URL to production

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://lethrent-app.onrender.com/api";
 
 // --- Helper Functions ---
 const showNotification = (message, isError = false) => {
